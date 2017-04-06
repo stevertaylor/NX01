512f282ccc1ff22c6e44a13d839fa7d88f4d0d76
Stephen R Taylor <steve.taylor1987@gmail.com>
2017-04-06 14:19:17 -0700

    adding planet mass priors draws from iau uncertainties

--- NX01_master.py
+++ NX01_master.py
@@ -6257,7 +6257,7 @@ elif args.sampler == 'ptmcmc':
                 mu = 0.0
                 sig = np.array([7.71489350e-12,4.79352991e-14,6.31466493e-15,
                                 2.08290722e-15,1.54976690e-11,8.17306184e-12,
-                                5.71923361e-11,7.96103855e-11,1.50162644e-12]))
+                                5.71923361e-11,7.96103855e-11,1.50162644e-12]])
                 q[pct+ind] = mu + np.random.randn() * sig[ind]
                 qxy -= (mu - parameters[pct+ind]) ** 2 / 2 / \
                   sig[ind] ** 2 - (mu - q[pct+ind]) ** 2 / 2 / sig[ind] ** 2
