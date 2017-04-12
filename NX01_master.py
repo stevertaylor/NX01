b0b81299159a0241e5ad4a73f9e17204153c00ab
Stephen R Taylor <steve.taylor1987@gmail.com>
2017-04-12 16:18:16 -0700

    adding fisher matrix draws for ephemeris quadratic

--- NX01_master.py
+++ NX01_master.py
@@ -1115,7 +1115,7 @@ if args.det_signal:
         if args.bwm_model_select:
             pmin = np.append(pmin,-0.5)
     if args.eph_quadratic:
-        pmin = np.append(pmin,-1e-11*np.ones(9)) # amps
+        pmin = np.append(pmin,-1e-8*np.ones(9)) # amps
         #pmin = np.append(pmin,np.tile([-10.0,-10.0],3)) # amps
         #pmin = np.append(pmin,np.tile([-1.0,-1.0],3)) # signs
     if args.eph_planetdelta:
@@ -1274,7 +1274,7 @@ if args.det_signal:
         if args.bwm_model_select:
             pmax = np.append(pmax,1.5)
     if args.eph_quadratic:
-        pmax = np.append(pmax,1e-11*np.ones(9)) # amps
+        pmax = np.append(pmax,1e-8*np.ones(9)) # amps
         #pmax = np.append(pmax,np.tile([0.0,0.0],3)) # amps
         #pmax = np.append(pmax,np.tile([1.0,1.0],3)) # signs
     if args.eph_planetdelta:
@@ -3977,7 +3977,7 @@ elif args.sampler == 'ptmcmc':
             if args.bwm_model_select:
                 x0 = np.append(x0,0.4)
         if args.eph_quadratic:
-            x0 = np.append(x0,np.random.uniform(-1e-11,1e-11,9))
+            x0 = np.append(x0,np.zeros(9))
             #x0 = np.append(x0,np.array(np.tile([-7.0],6)))
             #x0 = np.append(x0,np.random.uniform(-1.0,1.0,6))
         if args.eph_planetdelta:
@@ -6968,7 +6968,7 @@ elif args.sampler == 'ptmcmc':
             sampler.addProposalToCycle(drawFromBWMModelIndexPrior, 5)
     if args.det_signal and args.eph_quadratic:
         sampler.addProposalToCycle(drawFromEphemQuadPrior, 10)
-    #    sampler.addProposalToCycle(drawFromEphemQuadFisherPrior, 40)
+        sampler.addProposalToCycle(drawFromEphemQuadFisherPrior, 20)
     if args.det_signal and args.eph_planetdelta:
         if args.eph_planetmass:
             sampler.addProposalToCycle(drawFromEphPlanetDeltaPrior, 10)
